aba9249bc2737d62291f9de8eeb29770109ad2e1
Anshuman Rathore <anshumansr.cs.20@nitj.ac.in>
2023-09-28 22:06:30 +0530

    First Commit

---
+++ src/components/AddBudgetForm.jsx
@@ -0,0 +1,74 @@
+// reacts
+import { useEffect, useRef } from "react";
+
+// rrd imports
+import { Form, useFetcher } from "react-router-dom"
+
+// library imports
+import { CurrencyDollarIcon } from "@heroicons/react/24/solid"
+
+const AddBudgetForm = () => {
+  const fetcher = useFetcher();
+  const isSubmitting = fetcher.state === "submitting"
+
+  const formRef = useRef();
+  const focusRef = useRef();
+
+  useEffect(() => {
+    if (!isSubmitting) {
+      formRef.current.reset()
+      focusRef.current.focus()
+    }
+  }, [isSubmitting])
+
+  return (
+    <div className="form-wrapper">
+      <h2 className="h3">
+        Create budget
+      </h2>
+      <fetcher.Form
+        method="post"
+        className="grid-sm"
+        ref={formRef}
+      >
+        <div className="grid-xs">
+          <label htmlFor="newBudget">Budget Name</label>
+          <input
+            type="text"
+            name="newBudget"
+            id="newBudget"
+            placeholder="e.g., Groceries"
+            required
+            ref={focusRef}
+          />
+        </div>
+        <div className="grid-xs">
+          <label htmlFor="newBudgetAmount">Amount</label>
+          <input
+            type="number"
+            step="0.01"
+            name="newBudgetAmount"
+            id="newBudgetAmount"
+            placeholder="e.g., 500"
+            required
+            inputMode="decimal"
+          />
+        </div>
+        <input type="hidden" name="_action" value="createBudget" />
+        <button type="submit" className="btn btn--dark" disabled={isSubmitting}>
+          {
+            isSubmitting ? <span>Submitting…</span> : (
+              <>
+                <span>Create budget</span>
+                <svg width={20} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                  <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM9 7.5A.75.75 0 009 9h1.5c.98 0 1.813.626 2.122 1.5H9A.75.75 0 009 12h3.622a2.251 2.251 0 01-2.122 1.5H9a.75.75 0 00-.53 1.28l3 3a.75.75 0 101.06-1.06L10.8 14.988A3.752 3.752 0 0014.175 12H15a.75.75 0 000-1.5h-.825A3.733 3.733 0 0013.5 9H15a.75.75 0 000-1.5H9z" clipRule="evenodd" />
+                </svg>
+              </>
+            )
+          }
+        </button>
+      </fetcher.Form>
+    </div>
+  )
+}
+export default AddBudgetForm
